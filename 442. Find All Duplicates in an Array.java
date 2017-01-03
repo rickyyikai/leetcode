ef7ba2b6c2e53ded866365ebd99567e4f06a6256
Kai <rickyyikaichen@gmail.com>
2017-01-03 09:10:50 +0800

    Update 442. Find All Duplicates in an Array.java
    edit comments

--- 442. Find All Duplicates in an Array.java
+++ 442. Find All Duplicates in an Array.java
@@ -1,7 +1,7 @@
 public class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> list = new ArrayList<Integer>();
-      
+        
         for(int i=0;i<nums.length;i++){
             if(nums[Math.abs(nums[i])-1]>0){
                 nums[Math.abs(nums[i])-1]=-nums[Math.abs(nums[i])-1];
@@ -11,70 +11,25 @@ public class Solution {
             }
         }
         
-       /*雖然accepted，但遇到特殊test可能會出錯，且用到1個額外空間 
-       for(int i=0;i<nums.length;i++){
-            int temp=0;
-            if(nums[i]>0){
-            if(nums[nums[i]-1]>0){
-                temp=nums[i];
-                nums[i]=nums[nums[i]-1];
-                nums[temp-1]=0;
-             }
-                 
-            else if(nums[nums[i]-1]==0){
-                     nums[nums[i]-1]=-1;
-                 }
-            }
-            
-        }
-        
-        for(int i=0;i<nums.length;i++){
-            int temp=0;
-            if(nums[i]>0){
-            if(nums[nums[i]-1]>0){
-                temp=nums[i];
-                nums[i]=nums[nums[i]-1];
-                nums[temp-1]=0;
-             }
-                 
-            else if(nums[nums[i]-1]==0){
-                     nums[nums[i]-1]=-1;
-                 }
-            }
-            
-        }
-        
-        for(int i=0;i<nums.length;i++){
-            int temp=0;
-            if(nums[i]>0){
-            if(nums[nums[i]-1]>0){
-                temp=nums[i];
-                nums[i]=nums[nums[i]-1];
-                nums[temp-1]=0;
-             }
-                 
-            else if(nums[nums[i]-1]==0){
-                     nums[nums[i]-1]=-1;
-                 }
-            }
-            
-        }
+       /*雖然accepted，但遇到特殊test可能會出錯，且用到1個額外空間
+       for(int i=0;i<4;i++){
             for(int i=0;i<nums.length;i++){
-            int temp=0;
-            if(nums[i]>0){
-            if(nums[nums[i]-1]>0){
-                temp=nums[i];
-                nums[i]=nums[nums[i]-1];
-                nums[temp-1]=0;
-             }
-                 
-            else if(nums[nums[i]-1]==0){
+                int temp=0;
+                if(nums[i]>0){
+                    if(nums[nums[i]-1]>0){
+                        temp=nums[i];
+                        nums[i]=nums[nums[i]-1];
+                        nums[temp-1]=0;
+                    } 
+                    else if(nums[nums[i]-1]==0){
                      nums[nums[i]-1]=-1;
-                 }
-            }
+                    }
+                }
             
+            }
         }
-            for(int i=0;i<nums.length;i++){
+          
+        for(int i=0;i<nums.length;i++){
             if(nums[i]==-1)
             list.add(i+1);
         }
